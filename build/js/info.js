5e595b03de0efc902065d156c6b324c5bdc866ab
apeklets <damen.wouter@gmail.com>
2016-06-11 13:13:24 +0200

    Fix info.js

--- build/js/info.js
+++ build/js/info.js
@@ -1,6 +1,6 @@
 var BetterMagisterInfo = {
 	metroVersion: "Metro v1.4",
-	darkmodeVersion: "Dark Mode 1.6.5"
-	bmVersion: "v1.11"
+	darkmodeVersion: "Dark Mode 1.6.5",
+	bmVersion: "v1.11",
 	bmVersionx: "v1.11build2"
 };
