a38392f7c046aeec63f55d8902e261e4adbd289a
apeklets <damen.wouter@gmail.com>
2016-06-12 12:09:31 +0200

    Update info.js

--- build/js/info.js
+++ build/js/info.js
@@ -1,6 +1,6 @@
 var BetterMagisterInfo = {
 	metroVersion: "Metro v1.4.1",
-	darkmodeVersion: "Dark Mode 1.6.5"
-	bmVersion: "v1.10.1"
+	darkmodeVersion: "Dark Mode 1.6.5",
+	bmVersion: "v1.10.1",
 	bmVersionx: "v1.10.1"
 };
