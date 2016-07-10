cd562f31e6d27d6f09fc3fef603b94e647729f86
apeklets <damen.wouter@gmail.com>
2016-07-10 20:17:41 +0200

    Remove info.js to add updateChecker.js

--- build/js/info.js
+++
@@ -1,6 +0,0 @@
-var BetterMagisterInfo = {
-	metroVersion: "Metro v1.4.3",
-	darkmodeVersion: "Dark Mode 1.7",
-	bmVersion: "v1.10.1",
-	bmVersionx: "v1.10.1"
-};
---
+++ build/js/updateChecker.js
@@ -0,0 +1,14 @@
+var BetterMagisterInfo = {
+	metroVersion: "Metro v1.4.3",
+	darkmodeVersion: "Dark Mode 1.7",
+	bmVersion: "v1.10.1",
+	bmVersionx: "v1.10.1"
+};
+
+var updateAlert = function() {
+	var updateNotification = $('<div id="updateMsg" class="toast ng-scope info-toast info-msg" style="z-index: 999999;" data-ng-repeat="toast in activeToasts"><span class="glyph" id="updateGlyph"><a href="#"></a></span><span class="ng-binding" data-ng-bind="toast.toast.title">UPDATE!</span><em class="ng-binding" data-ng-bind-html="toast.toast.trustedMessage">Update voor BetterMagister beschikbaar!</em><a href="https://github.com/apeklets/BetterMagister/raw/gh-pages/build/gmBetterMagister2.user.js">Install</a> <a href="">Changelog</a> <a>Sluiten</a><i></i></div>')
+	updateNotification.appendTo('.toasts');
+	$('#updateMsg a').click(function() {
+		$('#updateMsg').remove();
+	})
+};
