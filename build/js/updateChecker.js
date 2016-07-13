3c1732018879c0c1ba85761ebc0e02d14f3ee393
apeklets <damen.wouter@gmail.com>
2016-07-13 15:05:12 +0200

    Change install link to open in new page

--- build/js/updateChecker.js
+++ build/js/updateChecker.js
@@ -5,7 +5,7 @@ var BetterMagisterInfo = {
 };
 
 var updateAlert = function() {
-	var updateNotification = $('<div id="updateMsg" class="toast ng-scope info-toast info-msg" style="z-index: 999999;" data-ng-repeat="toast in activeToasts"><span class="glyph" id="updateGlyph"><a href="#"></a></span><span class="ng-binding" data-ng-bind="toast.toast.title">UPDATE!</span><em class="ng-binding" data-ng-bind-html="toast.toast.trustedMessage">Update voor BetterMagister beschikbaar!</em><a href="https://apeklets.github.io/BetterMagister/#installatie">Install</a> <a href="https://apeklets.github.io/BetterMagister/#changelog" target="_blank">Changelog</a> <a>Sluiten</a><i></i></div>')
+	var updateNotification = $('<div id="updateMsg" class="toast ng-scope info-toast info-msg" style="z-index: 999999;" data-ng-repeat="toast in activeToasts"><span class="glyph" id="updateGlyph"><a href="#"></a></span><span class="ng-binding" data-ng-bind="toast.toast.title">UPDATE!</span><em class="ng-binding" data-ng-bind-html="toast.toast.trustedMessage">Update voor BetterMagister beschikbaar!</em><a href="https://apeklets.github.io/BetterMagister/#installatie" target="_blank">Install</a> <a href="https://apeklets.github.io/BetterMagister/#changelog" target="_blank">Changelog</a> <a>Sluiten</a><i></i></div>')
 	updateNotification.appendTo('.toasts');
 	$('#updateMsg a').click(function() {
 		$('#updateMsg').remove();
