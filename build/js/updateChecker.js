1748c956256ad661d69c21687b737aacc4da2eeb
apeklets <damen.wouter@gmail.com>
2016-07-13 15:02:12 +0200

    Update for v1.11.5

--- build/js/updateChecker.js
+++ build/js/updateChecker.js
@@ -1,11 +1,11 @@
 var BetterMagisterInfo = {
 	metroVersion: "Metro v1.5.1",
 	darkmodeVersion: "Dark Mode 1.7.1",
-	bmVersion: "v1.11.4",
+	bmVersion: "v1.11.5",
 };
 
 var updateAlert = function() {
-	var updateNotification = $('<div id="updateMsg" class="toast ng-scope info-toast info-msg" style="z-index: 999999;" data-ng-repeat="toast in activeToasts"><span class="glyph" id="updateGlyph"><a href="#"></a></span><span class="ng-binding" data-ng-bind="toast.toast.title">UPDATE!</span><em class="ng-binding" data-ng-bind-html="toast.toast.trustedMessage">Update voor BetterMagister beschikbaar!</em><a href="https://github.com/apeklets/BetterMagister/raw/gh-pages/build/gmBetterMagister2.user.js">Install</a> <a href="https://apeklets.github.io/BetterMagister/#changelog" target="_blank">Changelog</a> <a>Sluiten</a><i></i></div>')
+	var updateNotification = $('<div id="updateMsg" class="toast ng-scope info-toast info-msg" style="z-index: 999999;" data-ng-repeat="toast in activeToasts"><span class="glyph" id="updateGlyph"><a href="#"></a></span><span class="ng-binding" data-ng-bind="toast.toast.title">UPDATE!</span><em class="ng-binding" data-ng-bind-html="toast.toast.trustedMessage">Update voor BetterMagister beschikbaar!</em><a href="https://apeklets.github.io/BetterMagister/#installatie">Install</a> <a href="https://apeklets.github.io/BetterMagister/#changelog" target="_blank">Changelog</a> <a>Sluiten</a><i></i></div>')
 	updateNotification.appendTo('.toasts');
 	$('#updateMsg a').click(function() {
 		$('#updateMsg').remove();
